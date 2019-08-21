6e44f7335a78ea0ee678f8881fc20fda51099b09
unknown <contato@gilbertoalbino.com>
2019-08-21 00:24:45 -0300

    Adiciona arquivos para publicação no registro NPM

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * AppContent é um CMS alternativo
+ * para gestão de conteúdo na internet.
+ * 
+ * Este projeto é OpenSource, 
+ * se você gostaria de contribuir visite
+ * https://appcontent.github.io
+ * para mais informações.
+ * 
+ * @author Gilberto Albino
+ * @since 2019-08-20
+ */
